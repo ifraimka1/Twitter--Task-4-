e7c9c7e736f5fd14962ed26528d7aa66af6f13ba
Ifraim Solomonov <mr.ifraim@yandex.ru>
2023-04-24 00:10:44 +0300

    Авторизация, удаление/редактирование постов

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { Sequelize, DataTypes } = require('sequelize');
+
+const userRoutes = require('./user');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
 
 const app = express();
 const port = 3000;
 
-var { Liquid } = require('liquidjs');
-var engine = new Liquid();
-app.use(bodyParser.urlencoded());
+const { Liquid } = require('liquidjs');
+const engine = new Liquid();
 app.engine('liquid', engine.express());
 app.set('views', './views');            // specify the views directory
 app.set('view engine', 'liquid');       // set liquid to default
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static('./'));
 
+/**
+ * Блок инициализации БД.
+ * 
+ * В данном блоке происходит подключение к БД, проверка подключения, а также создание моделей.
+ */
 
+// Формат ссылки: движок://логин:пароль@хост:порт/базаданных.
 const sequelize = new Sequelize('postgres://postgres:Bahfbv!1037@localhost:5432/postgres');
 
+// Обернем функцию аутентификации в асинхронную.
 const authenticate = async () => {
     try {
-      await sequelize.authenticate();
-      console.log("Connection has been established successfully.");
+        await sequelize.authenticate();
+        console.log("Есть контакт.");
     } catch (error) {
-      console.error("Unable to connect to the database:", error);
+        console.error("Хьюстон, у нас проблема:", error);
     }
 };
 
+// Проверяем подключение к БД.
 authenticate();
 
+/**
+ * Модель пользователя, содержит поля:
+ * login - логин пользователя | строка
+ * name - имя | строка
+ * password - пароль | строка
+ * isAdmin - флаг, характеризующий наличие у пользователя админских прав | boolean
+ */
 const User = sequelize.define('User', {
     login: {
         type: DataTypes.STRING,
@@ -46,6 +70,11 @@ const User = sequelize.define('User', {
     }
 });
 
+/**
+ * Модель поста, содержит поля:
+ * userID - ID пользователя | внешний ключ на User
+ * content - содержание поста | строка
+ */
 const Post = sequelize.define('Post', {
     userID: {
         type: DataTypes.INTEGER,
@@ -61,25 +90,108 @@ const Post = sequelize.define('Post', {
 User.hasMany(Post, { foreignKey: 'userID' }); // У пользователя может быть много постов
 Post.belongsTo(User, { foreignKey: 'userID' }); // Пост принадлежит пользователю
 
-app.get('/', async (req, res) => {
-    // const tasks = await Task.findAll();
-    // const model = {
-    //     active: tasks.filter(x => !x.completed)
-    //         .map(x => x.get({plain:true})),
-    //     completed: tasks.filter(x => x.completed)
-    //         .map(x => x.get({plain:true}))
-    // }
-    res.render('home');
+/**
+ * Блок авторизации.
+ * 
+ * В данном блоке реализована авторизация пользователей.
+ */
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+    async function (username, password, done) {
+        const user = await User.findOne({ where: { login: username } });
+        console.log(user);
+        if (user && user.password == password) {
+            done(null, {
+                id: user.id,
+                login: user.login,
+                name: user.name,
+                isAdmin: user.isAdmin
+            });
+        } else {
+            done(null, false);
+        }
+    }
+));
+
+function auth(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local',
+    {
+        failureRedirect: '/login'
+    }),
+    (req, res) => {
+        // NOT OK
+        res.redirect('/');
+    });
+
+app.get('/logout', (req, res) => {
+    req.logout(() => {
+        res.redirect('/login');
+    })
+});
+
+app.get('/admin', (req, res) => {
+    res.render('admin', {
+        role: req.user.role,
+        userList: userList
+    });
+});
+
+app.get('/', auth, async (req, res) => {
+    const posts = await Post.findAll({ include: User, order: [['id', 'DESC']] });
+    res.render('home', {
+        name: req.user.name,
+        userID: req.user.id,
+        isAdmin: req.user.isAdmin,
+        posts: posts
+    });
 });
 
 app.post('/create', async (req, res) => {
-    // const task = new Task();
-    // task.name = req.body.name;
-    // await task.save();
+    const post = new Post();
+    post.userID = req.user.id;
+    post.content = req.body.content;
+    await post.save();
     res.redirect('/');
 });
 
-app.post('/update', async (req, res) => {
+app.post('/edit', async (req, res) => {
+    const postToEditID = Object.keys(req.body)
+        .filter(x=>x.startsWith('edit_'))
+        .map(x=>x.split('_')[1])[0];
+
+    const newContent = req.body['edit_'+postToEditID];
+
+    console.log(postToEditID, newContent);
+
+    await Post.update({ content: newContent }, {
+        where: {
+          id: postToEditID
+        }
+      });
+    
+    //const postToDel = await Post.findByPk(postToDelID);
+    //await postToDel.destroy();
+
     // const completedKeys = Object.keys(req.body)
     //     .filter(x=>x.startsWith('complete_'))
     //     .map(x => x.split('_')[1]);
@@ -102,7 +214,27 @@ app.post('/update', async (req, res) => {
     res.redirect('/');
 });
 
+app.post('/delete', async (req, res) => {
+    const postToDelID = Object.keys(req.body)
+        .filter(x=>x.startsWith('delete_'))
+        .map(x=>x.split('_')[1])[0];
+    
+    const postToDel = await Post.findByPk(postToDelID);
+    await postToDel.destroy();
+
+    res.redirect('/');
+});
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
 // User.sync({force:true});
 // Post.sync({force:true});
+app.use('/user', auth, userRoutes);
 
 app.listen(port, () => console.log('started'));
